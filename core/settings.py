98c12715a18529ba453d87544d90d9c95bc807ff
Muhammed fanish <muhammed.fanish@ibm.com>
2026-04-25 01:38:16 +0530

    settings modified

--- core/settings.py
+++ core/settings.py
@@ -118,10 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'booking' / 'static',
 ]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
